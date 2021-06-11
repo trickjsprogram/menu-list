0ccb23eb9bbe3adf51240646f69ddfd24dec64dd
Vishal <trickyjs091991@gmail.com>
2021-06-12 00:23:52 +0530

    restaurent menu list app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+import logo from "./logo.JPG";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <img src={logo} alt="logo" className="logo" />
+          <h2>Menu List</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Categories.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Categories = ({ categories, filterItems, activeCategory }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className={`${
+              activeCategory === category ? "filter-btn active" : "filter-btn"
+            }`}
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
